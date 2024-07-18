3bd8e3a60662f0b0422386603e347e73ba645c61
Gabriella Ketelly <164022146+gabriellaketelly@users.noreply.github.com>
2024-07-18 17:58:27 -0300

    Create 1318_bilhetesFalsos.c

---
+++ 1318_bilhetesFalsos.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+
+int main(){
+    
+    int bilhetes, convidados;
+    
+
+    while (scanf("%d %d", &bilhetes, &convidados) && bilhetes > 0 && convidados > 0) {
+
+        double quant[20001]={0};
+        int contador = 0;
+        int numero;
+
+        for(int i = 0; i < convidados ; i++) {
+            scanf("%d", &numero);
+            quant[numero]+=1;
+        }
+
+        for(int i = 0; i <= 20000; i++) {
+            if(quant[i]>1)
+            contador++;
+        }
+
+        printf("%d\n", contador);
+
+    }
+
+    return 0;
+
+}

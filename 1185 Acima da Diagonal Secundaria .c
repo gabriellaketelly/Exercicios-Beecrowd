e74dba1da694296caa4b1e950ae585d56d7e01d0
Gabriella Ketelly <164022146+gabriellaketelly@users.noreply.github.com>
2024-07-23 18:41:42 -0300

    Create 1185 Acima da Diagonal Secundaria .c

---
+++ 1185 Acima da Diagonal Secundaria .c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+
+    float matriz[12][12], soma = 0, media, elemento;
+    char operacao;
+
+    
+    scanf("%c", &operacao);
+
+    
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            scanf("%f", &elemento);
+            matriz[i][j] = elemento;
+        }
+    }
+
+    
+    for (int i = 0; i < 12; i++) 
+        for (int j = 0; j < 12; j++) 
+            if (i + j < 11) 
+                soma += matriz[i][j];
+            
+        
+    
+
+    
+    if (operacao == 'S') 
+        printf("%.1f\n", soma); 
+    else if (operacao == 'M') 
+        media = soma / 66; 
+        printf("%.1f\n", media); 
+    
+
+    return 0;
+}
